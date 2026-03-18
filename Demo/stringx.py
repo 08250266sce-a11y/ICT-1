8d6c54ef2bd80b4a6568090eff58017ccfaa761d
unknown <08250266.sce@rub.edu.bt>
2026-03-18 09:31:03 +0530

    this is first trial

---
+++ Demo/stringx.py
@@ -0,0 +1,12 @@
+print("Hello")
+print
+multiplelines = '''This is a string
+ that spans multiple lines.'''
+print(multiplelines)
+a = 1
+b = 2
+c = a + b
+print(type(c))
+c = str(c)
+print(type(c))
+print(c)
