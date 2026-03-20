4b035bcf52941a5f686704e5ce0c0420464564c5
unknown <08250266.sce@rub.edu.bt>
2026-03-20 15:49:16 +0530

    student activity

---
+++ Demo/activity.py
@@ -0,0 +1,10 @@
+name = str(input("what is your name: "))
+num1 = float(input("Enter your first number;"))
+num2 = float(input("Enter your second number;"))
+addition = num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+exponentiation = num1 ** num2
+print("Good Morning", name)
+print(addition,substraction,multiplication,division,exponentiation)
