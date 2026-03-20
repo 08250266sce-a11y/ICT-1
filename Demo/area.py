66b7b7bb654fdc67e10e4211f40aff3ac23f3281
unknown <08250266.sce@rub.edu.bt>
2026-03-20 15:12:48 +0530

    this is an example problrm

---
+++ Demo/area.py
@@ -0,0 +1,6 @@
+L = float(input("Enter the length of the rectangle:"));
+B = float(input("Enter the breath of the rectangle:"));
+area = L * B;
+print("The area of the rectangle is: ", area);
+print(type(L))
+print(type(B))
