45ce2027c861e94208ec91e4c2bd90e3648b00d6
unknown <08250266.sce@rub.edu.bt>
2026-03-24 21:21:06 -0600

    conditional operator

--- Demo/conditionaloperator.py
+++ Demo/conditionaloperator.py
@@ -1,16 +1,17 @@
 marks1 = float(input("Enter the marks of first subject: "))
 marks2 = float(input("Enter the marks of second subject: "))
 marks3 = float(input("Enter the marks of third subject: "))
-average = (marks1 + marks2 + marks3) / 3
-if (average >= 90 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
+Average = (marks1 + marks2 + marks3) / 3
+print("Averge:",Average)
+if (Average >= 90 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
     print("Grade: A")
-elif(average >= 80 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
+elif(Average >= 80 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
     print("Grade: B")
-elif(average >= 70 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
+elif(Average >= 70 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
     print("Grade: C")
-elif(average >= 60 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
+elif(Average >= 60 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
     print("Grade: D")
-elif(average >= 50 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
+elif(Average >= 50 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
     print("Grade: E")
 else:
     print("You need to work harder")
