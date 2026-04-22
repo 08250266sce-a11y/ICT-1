7ad7bc563f8daef193b2e0751ed7993409a1c37e
unknown <08250266.sce@rub.edu.bt>
2026-04-21 22:02:39 -0600

    adding the unit 2 on function

---
+++ Demo/unit 2/functionx.py
@@ -0,0 +1,24 @@
+print()
+# def sum ():
+    # a = 5
+    # b = 10
+    # print("The sum of the a and b is:", a + b )
+# def product():
+    # a = 5
+    # b = 10
+    # print("The product of a and b is:", a * b )
+# sum()
+# product()
+def sum_with_parameters(x, y):
+    print("The sum of ", x,"and", y, "is:", x + y)
+sum_with_parameters(3, 7)
+def product_with_parameters(x, y):
+    print("The product of ", x,"and", y, "is:", x * y)
+product_with_parameters(3, 7)
+def sum_with_return(x, y):
+    return x + y
+print("The sum of 4 and 6 is:", sum_with_return(4, 6))
+def product_with_return(a, b):
+    return a * b
+print("The product of 4 and 6 is:", product_with_return(4, 6))
+print()
