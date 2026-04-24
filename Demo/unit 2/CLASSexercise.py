f15e0cef54d9ab43f1e7abe3485ed9a33a121d2b
unknown <08250266.sce@rub.edu.bt>
2026-04-24 03:01:28 -0600

    adding exercise work

--- Demo/unit 2/CLASSexercise.py
+++ Demo/unit 2/CLASSexercise.py
@@ -8,14 +8,20 @@ def result(average):
         return "Pass"
     else:
         return "Fail"
-m1 = int(input("Enter marks of subject 1: "))
-m2 = int(input("Enter marks of subject 2: "))
-m3 = int(input("Enter marks of subject 3: "))
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+m1 = float(input("Enter marks of subject 1: "))
+m2 = float(input("Enter marks of subject 2: "))
+m3 = float(input("Enter marks of subject 3: "))
 total = calculate_total(m1, m2, m3)
 average = calculate_average(total)
 final_result = result(average)
-
-# print("Total Marks:", total)
+print("Total Marks:", total)
 print("Average Marks:", average)
 print("Result:", final_result)
+num = int(input("Enter a number to check even/odd: "))
+print("The number is:", check_even_odd(num))
 print()
