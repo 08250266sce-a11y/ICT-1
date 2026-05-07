be1a1c0a75bcb3b2ff355412c3f5fdb4b84099b9
unknown <08250266.sce@rub.edu.bt>
2026-05-07 07:57:12 -0600

    adding the float demo

--- Demo/floatx.py
+++ Demo/floatx.py
@@ -2,7 +2,7 @@ X = 1.10
 Y = 1.0
 Z = -35.59
 full_marks = 100
-print(type(full_marks))
-full_marks = float(full_marks)
-print(type(full_marks))
+print(type(full_marks)) #Data type of full_marks is int
+full_marks = float(full_marks) #Type casting
+print(type(full_marks)) #Data type of full marks is now float
 print(full_marks)
