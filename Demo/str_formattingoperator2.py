a6e0b1610bae04e0ad6400ed751a56f0cd1d701a
unknown <08250266.sce@rub.edu.bt>
2026-05-08 01:01:02 -0600

    adding the string formatting operator

--- Demo/str_formattingoperator2.py
+++ Demo/str_formattingoperator2.py
@@ -1,2 +1,4 @@
-print("we are in unit{}".format(2))
+print()
+print("we are in unit {}".format(2))
 print("{p} ia a programming language. with the help of {p} we can create various application.".format(p = "python"))
+print()
