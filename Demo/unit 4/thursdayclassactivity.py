4b5c73d7b42a14de9ff7bbc4592ef10e55ca340b
unknown <08250266.sce@rub.edu.bt>
2026-06-04 21:02:14 -0600

    adding while loop demo

---
+++ Demo/unit 4/thursdayclassactivity.py
@@ -0,0 +1,16 @@
+print()
+def print_pattern(n):
+    # Base case
+    if n == 0:
+        return
+    
+    # First recurse down
+    print_pattern(n - 1)
+    
+    # Then print stars after returning
+    print("* " * n)
+
+# Example usage
+n =int(input("Enter the number for n value: "))
+print_pattern(n) 
+print()
